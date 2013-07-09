a8268cf5894aeb2230f343d3da988f2c543d9a89
wangxuguang <ninesunqian@163.com>
2013-07-09 22:05:47 +0800

    add node list view

--- src/excitedmind/MindView.java
+++ src/excitedmind/MindView.java
@@ -3,12 +3,10 @@ package excitedmind;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
 import java.awt.event.MouseEvent;
 import java.util.Iterator;
 
-import javax.swing.AbstractAction;
 import javax.swing.KeyStroke;
 import javax.swing.SwingUtilities;
 import javax.swing.undo.UndoManager;
@@ -55,34 +53,7 @@ public class MindView extends Display {
     private MindTree m_mindTree;
     private TableNodeItem m_curFocus;
     
-    private double m_clickedItemX;
-    private double m_clickedItemY;
-    
     MindTreeRenderEngine m_renderEngine;
-    
-    
-    /** The data group name of menu items. */
-    public static final String ITEMS = "items";
-    /** The label data field for menu items. */
-    public static final String LABEL = "label";
-    /** The action data field for menu items. */
-    public static final String ACTION = "action";
-    /**
-     * This schema holds the data representation for internal storage of
-     * menu items.
-     */
-    protected static final Schema ITEM_SCHEMA = new Schema();
-    static {
-        ITEM_SCHEMA.addColumn(MindTree.sm_textPropName, String.class);
-    }
-    
-    private Table m_items = ITEM_SCHEMA.instantiate(); // table of menu items
-    
-    private double m_maxHeight = 500; // maximum menu height in pixels
-    private double m_scale = 7;       // scale parameter for fisheye distortion
-    
-    
-     
     
     public MindView(String path, Object rootId) {
         super(new Visualization());
@@ -97,58 +68,16 @@ public class MindView extends Display {
         m_vis.add(m_treeGroupName, m_mindTree.m_tree);
         setItemSorter(new TreeDepthItemSorter());
         
-        //history
-        m_vis.addTable(ITEMS, m_items);
-
         m_renderEngine = new MindTreeRenderEngine(this, m_treeGroupName);
         
         setMouseControlListener ();
         setKeyControlListener ();
         
         renderTree ();
-        
-        {
-        	addHistoryItem();
-        	// text color function
-        	// items with the mouse over printed in red, otherwise black
-        	
-        	ColorAction colors = new ColorAction(ITEMS, VisualItem.TEXTCOLOR);
-        	colors.setDefaultColor(ColorLib.gray(0));
-        	colors.add("hover()", ColorLib.rgb(255,0,0));
-
-        	// initial layout and coloring
-        	ActionList init = new ActionList();
-        	init.add(new VerticalLineLayout(ITEMS, m_maxHeight));
-        	init.add(colors);
-        	init.add(new RepaintAction());
-        	m_vis.putAction("init", init);
-
-        	// fisheye distortion based on the current anchor location
-        	ActionList distort = new ActionList();
-        	Distortion feye = new FisheyeDistortion(0,m_scale);
-        	distort.add(feye);
-        	distort.add(colors);
-        	distort.add(new RepaintAction());
-        	m_vis.putAction("distort", distort);
-
-        	// update the distortion anchor position to be the current
-        	// location of the mouse pointer
-        	addControlListener(new AnchorUpdateControl(feye, "distort"));	
-        	
-        	m_vis.run("init");
-        }
     }
     
     
     
-    //add history node
-    public void addHistoryItem() {
-    	for (int i=0; i<20; i++)
-    	{
-    		int row = m_items.addRow();
-    		m_items.set(row, MindTree.sm_textPropName, Integer.toString(i));
-    	}
-    }
     
     public boolean isNode (VisualItem item)
     {
@@ -166,6 +95,7 @@ public class MindView extends Display {
     }
     
     boolean m_needPan;
+    
     private void setMouseControlListener ()
     {
         addControlListener(new ZoomToFitControl());
@@ -173,44 +103,41 @@ public class MindView extends Display {
         addControlListener(new WheelZoomControl());
         addControlListener(new PanControl());
         addControlListener(new ControlAdapter() {
-        	
-        	//TODO
-//        addControlListener(new FocusControl(1, sm_layoutAction));
-        	public void itemEntered(VisualItem item, MouseEvent e) {
-        		if (isNode(item))
-        		{
-	    			System.out.println ("mouse entered");
-        			m_curFocus = (TableNodeItem)item;
-        			renderTree();
-        			
-        			Rectangle2D bounds = item.getBounds();
-        			MindTreeLayout.Params params = MindTreeLayout.getParams((NodeItem)item);
-        			System.out.println ("    mod = " + params.mod);
-        			System.out.println ("    prelim = " + params.prelim);
-        			System.out.println ("    breadth = " + params.breadth);
-        			System.out.println ("    bounds.y = " + bounds.getY());
-        			
-        			
-        		}
-        		
-        	}
-        	
-        	public void itemClicked(VisualItem item, MouseEvent e) {
+
+    		public void itemEntered(VisualItem item, MouseEvent e) {
+    			if (isNode(item))
+    			{
+    				System.out.println ("mouse entered");
+    				m_curFocus = (TableNodeItem)item;
+    				renderTree();
+
+    				Rectangle2D bounds = item.getBounds();
+    				MindTreeLayout.Params params = MindTreeLayout.getParams((NodeItem)item);
+    				System.out.println ("    mod = " + params.mod);
+    				System.out.println ("    prelim = " + params.prelim);
+    				System.out.println ("    breadth = " + params.breadth);
+    				System.out.println ("    bounds.y = " + bounds.getY());
+
+
+    			}
+
+    		}
+
+    		public void itemClicked(VisualItem item, MouseEvent e) {
     			System.out.println ("mouse Clicked");
     			m_curFocus = (TableNodeItem)item;
-    			
-        		if (isNode(item))
-        		{
-        			m_renderEngine.holdItem(item);
-    			
-	        		m_mindTree.ToggleFoldNode(item);
-        			renderTree();
-	        		
-        		}
-        	} 
-        }
-        );
-    	
+
+    			if (isNode(item))
+    			{
+    				m_renderEngine.holdItem(item);
+
+    				m_mindTree.ToggleFoldNode(item);
+    				renderTree();
+
+    			}
+    		} 
+    	}
+    	);
     }
     
     
@@ -264,67 +191,4 @@ public class MindView extends Display {
     	return m_curFocus;
     }
     
-    
-    
-    /** history
-     * Lines up all VisualItems vertically. Also scales the size such that
-     * all items fit within the maximum layout size, and updates the
-     * Display to the final computed size.
-     */
-    public class VerticalLineLayout extends Layout {
-        private double m_maxHeight = 600;
-        
-        public VerticalLineLayout(String group, double maxHeight) {
-        	super (group);
-            m_maxHeight = maxHeight;
-        }
-        
-        public void run(double frac) {
-            // first pass
-            double w = 0, h = 0;
-            Iterator iter = m_vis.items(m_group);
-            while ( iter.hasNext() ) {
-                VisualItem item = (VisualItem)iter.next();
-                item.setSize(1.0);
-                h += item.getBounds().getHeight();
-            }
-            double scale = h > m_maxHeight ? m_maxHeight/h : 1.0;
-            
-            Display d = m_vis.getDisplay(0);
-            Insets ins = d.getInsets();
-            
-            // second pass
-            h = ins.top;
-            double ih, y=0, x=ins.left;
-            iter = m_vis.items(m_group);
-            while ( iter.hasNext() ) {
-                VisualItem item = (VisualItem)iter.next();
-                item.setSize(scale); item.setEndSize(scale);
-                Rectangle2D b = item.getBounds();
-                
-                w = Math.max(w, b.getWidth());
-                ih = b.getHeight();
-                y = h+(ih/2);
-                setX(item, null, x);
-                setY(item, null, y);
-                h += ih;
-            }
-            
-            /*
-            // set the display size to fit text
-            setSize(d, (int)Math.round(2*m_scale*w + ins.left + ins.right),
-                       (int)Math.round(h + ins.bottom));
-                       */
-        }
-        
-        private void setSize(final Display d, final int width, final int height)
-        {
-        	SwingUtilities.invokeLater(new Runnable() {
-        		public void run() {
-        			d.setSize(width, height);
-        		}
-        	});
-        }
-    } // end of inner class VerticalLineLayout
-
 } // end of class TreeMap
