b47ea250ace836eeaefc5df2c60d27b103f7b499
wangxuguang <ninesunqian@163.com>
2014-12-22 23:42:13 +0800

    add MIT license

--- infinitemindmap/src/main/java/infinitmindmap/importer/TikaPlainTextImporter.java
+++ infinitemindmap/src/main/java/infinitmindmap/importer/TikaPlainTextImporter.java
@@ -1,27 +1,15 @@
 package infinitmindmap.importer;
 
-/**
- * Created with IntelliJ IDEA.
- * User: wangxuguang
- * Date: 14-10-7
- * Time: 上午6:28
- * To change this template use File | Settings | File Templates.
- */
 import com.ibm.icu.text.BreakIterator;
 import infinitmindmap.MindDB;
 import org.apache.commons.lang.StringUtils;
 import org.apache.tika.exception.TikaException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import java.io.File;
 import java.io.IOException;
-
 import javax.swing.tree.DefaultMutableTreeNode;
-
 import org.xml.sax.SAXException;
-
-
 import org.apache.tika.detect.DefaultDetector;
 import org.apache.tika.detect.Detector;
 import org.apache.tika.io.TikaInputStream;
@@ -33,6 +21,28 @@ import org.apache.tika.sax.BodyContentHandler;
 
 import java.io.*;
 import java.util.*;
+
+/*
+The MIT License (MIT)
+Copyright (c) 2012-2014 wangxuguang ninesunqian@163.com
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
 
 public class TikaPlainTextImporter extends Importer{
 
