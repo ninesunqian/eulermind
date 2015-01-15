f72cfde8bbe97157a9ccbe0ec7906e6a70d1ce01
wangxuguang <ninesunqian@163.com>
2015-01-15 22:27:49 +0800

    restruct the packages

---
+++ eulermind/src/main/java/eulermind/view/MindView.java
@@ -0,0 +1,522 @@
+package eulermind.view;
+
+import java.awt.event.*;
+
+import java.util.ArrayList;
+
+import eulermind.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.swing.*;
+
+import eulermind.operator.*;
+import prefuse.Display;
+import prefuse.Visualization;
+
+import prefuse.controls.*;
+import prefuse.data.*;
+import prefuse.util.PrefuseLib;
+import prefuse.visual.EdgeItem;
+import prefuse.visual.NodeItem;
+import prefuse.visual.VisualTree;
+import prefuse.visual.sort.TreeDepthItemSorter;
+
+/*
+The MIT License (MIT)
+Copyright (c) 2012-2014 wangxuguang ninesunqian@163.com
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+public class MindView extends Display {
+
+    final Logger m_logger = LoggerFactory.getLogger(this.getClass());
+    final String m_treeGroupName = "tree";
+
+    final public MindModel m_mindModel;
+    MindController m_mindController;
+
+    public TreeCursor m_cursor;
+    Node m_savedCursor = null;
+
+    TreeFolder m_folder;
+
+    public Tree m_tree;
+	MindTreeRenderEngine m_renderEngine;
+
+    MindEditor m_mindEditor;
+
+    boolean m_isChanging = false;
+
+    //提出这个函数是为了单元测试
+    void endInserting(String text)
+    {
+        MindOperator operator = new AddingChild(m_mindModel, getCursorSourceNode().getParent(), getCursorSourceNode().getIndex(), text);
+        m_logger.info("MindView fire OK, insert at {}", getCursorSourceNode().getIndex());
+        removePlaceholderCursor();
+
+        //该函数会重绘所有的MindView
+        m_mindController.does(operator);
+        hideEditor();
+
+    }
+
+    void endInserting(Object dbId, String text, Object parentDBId, String parentText)
+    {
+        MindOperator operator = new AddingReference(m_mindModel, getCursorSourceNode().getParent(),
+                dbId, getCursorSourceNode().getIndex());
+        removePlaceholderCursor();
+
+        //该函数会重绘所有的MindView
+        m_mindController.does(operator);
+        hideEditor();
+
+    }
+
+    void cancelInserting()
+    {
+        removePlaceholderCursor();
+
+        //重绘，并回到正常状态
+        renderTreeToEndChanging();
+        hideEditor();
+    }
+
+    MindEditor.MindEditorListener m_editorListenerForInserting = new MindEditor.MindEditorListener() {
+        public void editorOk(String text) {
+            endInserting(text);
+        }
+
+        public void promptListOk(Object dbId, String text, Object parentDBId, String parentText) {
+            endInserting(dbId, text, parentDBId, parentText);
+        }
+
+        public void cancel() {
+            cancelInserting();
+        }
+    };
+
+    void endEditing(String text)
+    {
+        MindOperator operator;
+
+        operator = new SettingProperty(m_mindModel, getCursorSourceNode(), MindModel.sm_textPropName, text);
+
+        m_mindController.does(operator);
+        hideEditor();
+
+    }
+
+    void cancelEditing()
+    {
+        hideEditor();
+    }
+
+    MindEditor.MindEditorListener m_editorListenerForEditing = new MindEditor.MindEditorListener() {
+        public void editorOk(String text) {
+            endEditing(text);
+        }
+
+        public void cancel() {
+            cancelEditing();
+        }
+    };
+
+    protected FocusListener m_mindEditorFocusListener = new FocusAdapter() {
+
+            @Override
+            public void focusLost(FocusEvent e) {
+                /*
+                if (isEditing()) {
+                    String text = getTextEditor().getText();
+                    m_mindEditorListener.editorOk(text);
+                }
+                */
+            }
+    };
+
+    VisualTree m_visualTree;
+
+	public MindView(MindModel mindModel, MindController undoManager, Object rootId) {
+		super(new Visualization());
+
+        //s_logger.setLevel(Level.OFF);
+		setSize(700, 600);
+		setHighQuality(true);
+
+        m_mindModel = mindModel;
+        m_mindController = undoManager;
+
+        m_tree = mindModel.findOrPutTree(rootId, 1);
+        m_visualTree = (VisualTree)m_vis.add(m_treeGroupName, m_tree);
+        MindModel.addNodeMirrorXYColumn(m_tree, m_visualTree);
+
+        setItemSorter(new TreeDepthItemSorter());
+        m_renderEngine = new MindTreeRenderEngine(this, m_treeGroupName);
+
+        m_cursor = new TreeCursor(this);
+        m_folder = new TreeFolder(this);
+
+		setMouseControlListener();
+
+        m_mindEditor = new MindEditor();
+        m_mindEditor.init(m_mindModel.m_mindDb);
+        m_mindEditor.setHasPromptList(true);
+
+        m_mindEditor.setBorder(null);
+        m_mindEditor.setVisible(false);
+
+        setTextEditor(m_mindEditor);
+        this.requestFocusInWindow();
+
+        m_mindEditor.addFocusListener(m_mindEditorFocusListener);
+        this.setFocusCycleRoot(true);
+
+	}
+
+    public NodeItem toVisual (Node node)
+    {
+        if (node instanceof NodeItem) {
+            return  (NodeItem) node;
+        } else {
+            String treeNodesGroupName = PrefuseLib.getGroupName(m_treeGroupName, Graph.NODES);
+            return (NodeItem) m_vis.getVisualItem(treeNodesGroupName, node);
+        }
+    }
+
+    public EdgeItem toVisual (Edge edge)
+    {
+        if (edge instanceof EdgeItem) {
+            return (EdgeItem) edge;
+        } else {
+            String treeEdgesGroupName = PrefuseLib.getGroupName(m_treeGroupName, Graph.EDGES);
+            return (EdgeItem) m_vis.getVisualItem(treeEdgesGroupName, edge);
+        }
+    }
+
+    public Node toSource (NodeItem nodeItem)
+    {
+        return (Node) m_vis.getSourceTuple (nodeItem);
+    }
+
+    public Edge toSource (EdgeItem edgeItem)
+    {
+        return (Edge) m_vis.getSourceTuple (edgeItem);
+    }
+
+	public void renderTree() {
+        if (m_renderEngine != null) {
+            m_renderEngine.run(null);
+        }
+	}
+
+    public void renderTree(Runnable runAfterRePaint) {
+        m_renderEngine.run(runAfterRePaint);
+    }
+
+    public void renderTreeToEndChanging() {
+        m_renderEngine.run(new Runnable() {
+            public void run() {
+                endChanging();
+            }
+        });
+    }
+
+    ControlAdapter m_zoomToFitControl;
+    ControlAdapter m_zoomControl;
+    ControlAdapter m_wheelZoomControl;
+    ControlAdapter m_panControl;
+
+    NodeDraggingControl m_dragControl;
+
+    public NodeItem getDragHitNode()
+    {
+        return m_dragControl.m_hitNode;
+    }
+
+	private void setMouseControlListener()
+    {
+		m_zoomToFitControl = new ZoomToFitControl(Control.MIDDLE_MOUSE_BUTTON);
+		m_zoomControl = new ZoomControl();
+		m_wheelZoomControl = new WheelZoomControl();
+		m_panControl = new PanControl();
+
+		m_dragControl = new NodeDraggingControl(this);
+
+        addControlListener(m_zoomToFitControl);
+        addControlListener(m_zoomControl);
+        addControlListener(m_wheelZoomControl);
+        addControlListener(m_panControl);
+
+        addControlListener(m_cursor);
+        addControlListener(m_dragControl);
+        addControlListener(m_folder);
+	}
+
+    void setTransformEnabled(boolean enabled)
+    {
+        m_zoomToFitControl.setEnabled(enabled);
+        m_zoomControl.setEnabled(enabled);
+        m_wheelZoomControl.setEnabled(enabled);
+        m_panControl.setEnabled(enabled);
+    }
+
+    private void showEditor()
+    {
+        editText(toVisual(getCursorSourceNode()), MindModel.sm_textPropName) ;
+    }
+
+    private void hideEditor()
+    {
+        super.stopEditing2(false);
+    }
+
+    public void setCursorNodeByPath(ArrayList<Integer> path)
+    {
+        m_cursor.setCursorNodeItem(toVisual(m_mindModel.getNodeByPath(m_tree, path)));
+    }
+
+    boolean beginChanging() {
+        if (m_isChanging) {
+            return false;
+        }
+
+        m_logger.info("beginChanging++++++++++++++++++++++");
+        m_cursor.hold();
+        setTransformEnabled(false);
+        m_isChanging = true;
+        return true;
+    }
+
+    void endChanging() {
+        if (! m_isChanging) {
+            return;
+        }
+        m_cursor.free();
+        setTransformEnabled(true);
+
+        m_logger.info("endChanging----------------------");
+        m_isChanging = false;
+    }
+
+    void beginAdding(boolean asChild)
+    {
+        if (! beginChanging()) {
+            return;
+        }
+
+        if (getCursorSourceNode() == m_tree.getRoot()) {
+            alert("you must open the root parent, before add a sibling of the root");
+            endChanging();
+            return;
+        }
+
+        m_logger.info("beginAdding---------------");
+
+        NodeItem cursorItem = m_cursor.getCursorNodeItem();
+        if (asChild) {
+            if (m_folder.isFolded(cursorItem)) {
+                m_folder.unfoldNode(cursorItem);
+            }
+        } else {
+            //TODO 防止 NodeIem与Node比较
+            if (toSource(cursorItem) == m_tree.getRoot()) {
+                assert(false);
+                return;
+            }
+        }
+
+        addPlaceholder(asChild);
+
+        renderTree(new Runnable() {
+            @Override
+            public void run() {
+                SwingUtilities.invokeLater(new Runnable() {
+                    @Override
+                    public void run() {
+                        m_logger.warn("++++++++++++++ show Editor");
+                        //TODO
+                        //listener 内部，调用endInstering (重绘，设置重绘后的hander是 恢复状态)
+                        m_mindEditor.setMindEditorListener(m_editorListenerForInserting);
+                        m_mindEditor.setHasPromptList(true);
+                        showEditor();
+                        m_logger.warn("--------------- after show Editor");
+                    }
+                });
+            }
+        });
+    }
+
+    public void importFile()
+    {
+        beginChanging();
+        JFileChooser chooser = new JFileChooser();
+        chooser.setMultiSelectionEnabled(false);
+        chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+        String path;
+
+        if (chooser.showOpenDialog(this) == JFileChooser.APPROVE_OPTION) {
+            path = chooser.getSelectedFile().getPath();
+            MindOperator operator = new ImportingFile(m_mindModel, getCursorSourceNode(), path);
+            m_mindController.does(operator);
+        }
+        endChanging();
+    }
+
+    private void alert(String msg)
+    {
+        JOptionPane.showMessageDialog(null, msg);
+    }
+
+    private void addPlaceholder(boolean asChild)
+    {
+        Node cursorNode = getCursorSourceNode();
+        m_savedCursor = cursorNode;
+
+        Node newNode;
+
+        if (asChild) {
+            newNode = m_tree.addChild(cursorNode, cursorNode.getChildCount());
+            m_folder.unfoldNode(toVisual(cursorNode));
+
+        } else {
+            newNode = m_tree.addChild(cursorNode.getParent(), cursorNode.getIndex() + 1);
+            m_logger.info("add sibling at {}", cursorNode.getIndex() + 1);
+        }
+
+        //NOTE: newNode.setString(MindModel.sm_textPropName, "") error
+
+        newNode.set(MindModel.sm_textPropName, "");
+
+        m_cursor.setCursorNodeItem(toVisual(newNode));
+    }
+
+    private void removePlaceholderCursor()
+    {
+        Node placeholderNode = getCursorSourceNode();
+        if (isPlaceholder(placeholderNode) == false) {
+            int i=0;
+        }
+        assert(isPlaceholder(placeholderNode));
+        assert(placeholderNode != m_tree.getRoot());
+
+        m_tree.removeChild(placeholderNode);
+        m_cursor.setCursorNodeItem(toVisual(m_savedCursor));
+    }
+
+    //include node and edge, the edge is used rendering
+    public boolean isPlaceholder(Tuple tuple)
+    {
+        return (m_mindModel.getDBId(tuple) == null);
+    }
+
+    public NodeItem getCursorNodeItem()
+    {
+        return m_cursor.getCursorNodeItem();
+    }
+
+    public Node getCursorSourceNode()
+    {
+        if (m_cursor.getCursorNodeItem() == null) {
+            int debug = 1;
+        }
+        return toSource(m_cursor.getCursorNodeItem());
+    }
+
+    public void setCursorProperty(String key, Object value)
+    {
+        //called by toolbar controls' action listener,
+        beginChanging();
+
+        Node cursorNode = getCursorSourceNode();
+        MindOperator operator = new SettingProperty(m_mindModel, cursorNode, key, value);
+
+        m_mindController.does(operator);
+    }
+
+
+    void undo() {
+        beginChanging();
+        if (m_mindController.canUndo()) {
+            m_mindController.undo();
+        } else {
+            endChanging();
+        }
+    }
+
+    void redo() {
+        beginChanging();
+        if (m_mindController.canRedo()) {
+            m_mindController.redo();
+        } else {
+            endChanging();
+        }
+    }
+
+    void save() {
+        m_mindModel.m_mindDb.commit();
+    }
+
+    public void remove()
+    {
+        beginChanging();
+        if (Removing.canDo(m_mindModel, m_tree, getCursorSourceNode())) {
+            Node cursorNode = getCursorSourceNode();
+            MindOperator operator = new Removing(m_mindModel, cursorNode);
+            m_mindController.does(operator);
+        } else {
+            endChanging();
+        }
+    }
+
+    public void addChild() {
+        beginAdding(true);
+    }
+
+    public void addSibling() {
+        beginAdding(false);
+    }
+
+
+    public void edit() {
+        beginChanging();
+        m_mindEditor.setMindEditorListener(m_editorListenerForEditing);
+        m_mindEditor.setHasPromptList(false);
+        showEditor();
+    }
+
+    public void cursorMoveUp() {
+        m_cursor.moveUp();
+    }
+
+    public void cursorMoveDown() {
+        m_cursor.moveDown();
+    }
+
+    public void cursorMoveLeft() {
+        m_cursor.moveLeft();
+    }
+    public void cursorMoveRight() {
+        m_cursor.moveRight();
+    }
+
+    public void toggleFoldNode() {
+        m_folder.toggleFoldNode(m_cursor.getCursorNodeItem());
+    }
+} // end of class TreeMap
---
+++ eulermind/src/main/java/eulermind/view/TreeCursor.java
@@ -0,0 +1,338 @@
+package eulermind.view;
+
+import prefuse.data.Table;
+import prefuse.util.collections.IntIterator;
+import prefuse.visual.NodeItem;
+import prefuse.visual.VisualTree;
+import prefuse.visual.expression.VisiblePredicate;
+
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+import java.awt.geom.Rectangle2D;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.swing.*;
+
+/*
+The MIT License (MIT)
+Copyright (c) 2012-2014 wangxuguang ninesunqian@163.com
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+public class TreeCursor extends NodeControl {
+
+    MindView m_mindView;
+    VisualTree m_tree;
+
+    NodeItem m_originCursor;
+
+    public NodeItem m_currentCursor;
+
+    ArrayList<NodeItem> m_xAxis;
+    ArrayList<NodeItem> m_yAxis;
+
+    int m_originXIndex;
+    int m_originYIndex;
+
+    int m_currentXIndex;
+    int m_currentYIndex;
+
+    boolean m_isHeld = false;
+
+    final Logger m_logger = LoggerFactory.getLogger(this.getClass());
+
+    public TreeCursor(MindView mindView) {
+        super(mindView);
+
+        m_mindView = mindView;
+        m_tree = mindView.m_visualTree;
+        setCursorNodeItem((NodeItem) m_tree.getRoot());
+    }
+
+    public NodeItem getCursorNodeItem()
+    {
+        return m_currentCursor;
+    }
+
+    public void setCursorNodeItem(NodeItem node)
+    {
+        m_originCursor = node;
+        m_currentCursor = node;
+
+        if (node == null) {
+            int debug = 1;
+        }
+
+        //build m_xAxis and m_yAxis by lazy mode.
+        //so we can setCurserNode before layout tree
+        m_xAxis = null;
+        m_yAxis = null;
+
+        m_mindView.renderTree();
+    }
+
+    private void buildXYAxis(NodeItem originCursor)
+    {
+        m_originCursor = originCursor;
+        m_currentCursor = originCursor;
+
+        m_xAxis = new ArrayList<NodeItem>();
+        m_yAxis = new ArrayList<NodeItem>();
+
+        NodeItem root = (NodeItem)m_tree.getRoot();
+        NodeItem lefter = m_originCursor;
+
+        //add m_originCursor and lefters
+        while (lefter != root) {
+            m_xAxis.add(0, lefter);
+            lefter = (NodeItem)lefter.getParent();
+        }
+        m_xAxis.add(0, root);
+
+        //add righters
+        NodeItem righterParent = m_originCursor;
+        while (righterParent.getChildCount() > 0 && righterParent.isExpanded() == true) {
+            Iterator children = righterParent.children();
+            NodeItem righter = null;
+
+            while (children.hasNext()) {
+                NodeItem child = (NodeItem)children.next();
+
+                if (righter == null ||
+                        Math.abs(child.getY() - righterParent.getY()) < Math.abs(righter.getY() - righterParent.getY())) {
+                    righter = child;
+                }
+            }
+            m_xAxis.add(righter);
+
+            righterParent = righter;
+        }
+
+        //fill uper and downer
+        Table nodeTable = m_tree.getNodeTable();
+        IntIterator allRows = nodeTable.rows(new VisiblePredicate());
+        NodeItem upDowner;
+
+        while (allRows.hasNext()) {
+            int row = allRows.nextInt();
+            upDowner = (NodeItem) m_tree.getNode(row);
+            if (overlayInXAxis(upDowner, m_originCursor) || upDowner.isExpanded() == false) {
+                if (overlayInXAxis(upDowner, m_originCursor)) {
+                    m_logger.debug(m_originCursor.getString("text") + ": overlay upDowner: " + upDowner.getString("text"));
+                }
+                m_yAxis.add(upDowner);
+            }
+        }
+
+        Collections.sort(m_yAxis, new Comparator<NodeItem>() {
+            @Override
+            public int compare(NodeItem n1, NodeItem n2) {
+                return (int)(n1.getY() - n2.getY());
+            }
+        });
+
+        m_originXIndex = m_xAxis.indexOf(originCursor);
+        m_originYIndex = m_yAxis.indexOf(originCursor);
+
+        m_currentXIndex = m_originXIndex;
+        m_currentYIndex = m_originYIndex;
+    }
+
+
+    boolean overlayInXAxis(NodeItem n1, NodeItem n2) {
+        Rectangle2D bounds1 = n1.getBounds();
+        Rectangle2D bounds2 = n2.getBounds();
+
+        double left1 = bounds1.getMinX();
+        double right1 = bounds1.getMaxX();
+
+        double left2 = bounds2.getMinX();
+        double right2 = bounds2.getMaxX();
+
+        if (left1 < left2) {
+            return left2 < right1;
+        } else {
+            return left1 < right2;
+        }
+    }
+
+    void moveLeft()
+    {
+        stopCursorTimer();
+
+        if (m_isHeld) {
+            return;
+        }
+
+        if (m_xAxis == null || m_currentYIndex != m_originYIndex) {
+            buildXYAxis(m_currentCursor);
+        }
+
+        if (m_currentXIndex > 0) {
+            m_currentXIndex--;
+
+            m_currentCursor = m_xAxis.get(m_currentXIndex);
+            m_mindView.renderTree();
+        }
+    }
+
+    void moveRight()
+    {
+        stopCursorTimer();
+
+        if (m_isHeld) {
+            return;
+        }
+
+        if (m_xAxis == null || m_currentYIndex != m_originYIndex) {
+            buildXYAxis(m_currentCursor);
+        }
+
+        if (m_currentXIndex < m_xAxis.size() - 1) {
+            //如果该节点闭合了，焦点就不能右移动了。
+            if (!m_currentCursor.isExpanded()) {
+                return;
+            }
+
+        } else if (m_currentXIndex == m_xAxis.size() - 1) {
+            //如果最右边的节点张开了，重新计算
+            if (m_currentCursor.getChildCount() > 0 && m_currentCursor.isExpanded()) {
+                buildXYAxis(m_originCursor);
+            }
+        }
+
+        if (m_currentXIndex < m_xAxis.size() - 1) {
+            m_currentXIndex++;
+
+            m_currentCursor = m_xAxis.get(m_currentXIndex);
+            m_mindView.renderTree();
+        }
+    }
+
+    void moveUp()
+    {
+        stopCursorTimer();
+
+        if (m_isHeld) {
+            return;
+        }
+
+        if (m_xAxis == null || m_currentXIndex != m_originXIndex) {
+            buildXYAxis(m_currentCursor);
+        }
+        if (m_currentYIndex > 0) {
+            m_currentYIndex--;
+
+            m_currentCursor = m_yAxis.get(m_currentYIndex);
+            m_mindView.renderTree();
+        }
+    }
+
+    void moveDown()
+    {
+        stopCursorTimer();
+
+        if (m_isHeld) {
+            return;
+        }
+
+        if (m_xAxis == null || m_currentXIndex != m_originXIndex) {
+            buildXYAxis(m_currentCursor);
+        }
+
+        if (m_currentYIndex < m_yAxis.size() - 1) {
+            m_currentYIndex++;
+
+            m_currentCursor = m_yAxis.get(m_currentYIndex);
+            m_mindView.renderTree();
+        }
+    }
+
+    @Override
+    public void nodeItemEntered(NodeItem item, MouseEvent e) {
+        if (m_isHeld) {
+            return;
+        }
+
+        startCursorTimer(item);
+    }
+
+    @Override
+    public void nodeItemExited(NodeItem item, MouseEvent e) {
+        stopCursorTimer();
+    }
+
+    @Override
+    public void nodeItemPressed(NodeItem item, MouseEvent e) {
+        stopCursorTimer();
+
+        if (m_isHeld) {
+            return;
+        }
+
+        if (item != m_currentCursor) {
+            setCursorNodeItem(item);
+            m_mindView.renderTree();
+        }
+    }
+
+    private Timer m_cursorTimer;
+
+    private void startCursorTimer(final NodeItem nodeItem)
+    {
+        m_cursorTimer = new Timer(500, new ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent actionEvent) {
+                setCursorNodeItem(nodeItem);
+            }
+        });
+        m_cursorTimer.setRepeats(false);
+        m_cursorTimer.setCoalesce(true);
+        m_cursorTimer.start();
+    }
+
+    private void stopCursorTimer()
+    {
+        if (m_cursorTimer != null) {
+            m_cursorTimer.stop();
+            m_cursorTimer = null;
+        }
+    }
+
+    @Override
+    public void nodeItemKeyPressed(NodeItem nodeItem, KeyEvent e) {
+        keyPressed(e);
+    }
+
+
+    public void hold() {
+        stopCursorTimer();
+        m_isHeld = true;
+    }
+
+    public void free() {
+        m_isHeld = false;
+    }
+
+}
