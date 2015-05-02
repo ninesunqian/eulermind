46405d7fa3bea665c058f7798658cd81b5bacb6d
wangxuguang <ninesunqian@163.com>
2015-05-02 21:35:04 +0800

    make jnlp package, but disable proguard

---
+++ utils/fix_sign_jar_bugs.sh
@@ -0,0 +1,3 @@
+XMLBEAN_DIR="~/.m2/repository/org/apache/xmlbeans/xmlbeans/2.6.0/"
+mv $XMLBEAN_DIR/xmlbeans-2.6.0.jar $XMLBEAN_DIR/xmlbeans-2.6.0.jar.bk
+cp unprocessed_xmlbeans__V2.6.0.jar $XMLBEAN_DIR/xmlbeans-2.6.0.jar
