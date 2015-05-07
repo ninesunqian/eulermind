f33833056580013b84bdf72dd6697733c0482196
wangxuguang <ninesunqian@163.com>
2015-05-08 05:25:57 +0800

    a temp commit

--- utils/fix_sign_jar_bugs.sh
+++ utils/fix_sign_jar_bugs.sh
@@ -1,3 +1,3 @@
-XMLBEAN_DIR="~/.m2/repository/org/apache/xmlbeans/xmlbeans/2.6.0/"
+XMLBEAN_DIR="${HOME}/.m2/repository/org/apache/xmlbeans/xmlbeans/2.6.0/"
 mv $XMLBEAN_DIR/xmlbeans-2.6.0.jar $XMLBEAN_DIR/xmlbeans-2.6.0.jar.bk
 cp unprocessed_xmlbeans__V2.6.0.jar $XMLBEAN_DIR/xmlbeans-2.6.0.jar
