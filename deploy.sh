85faf37dd69e4765c2f15172f62e6659fce3957a
wangxuguang <ninesunqian@163.com>
2015-05-09 22:40:58 +0800

    change the keypass and storepass argument

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,21 @@
-
-
-cd eulermind/target/jnlp/
-
-
-
 function make_jnlp ()
 {
-    new_jnlp=""$1.jnlp
+    new_jnlp="$1.jnlp"
+    host="$2"
 
-    sed 's/${project.url}/http:\/\/localhost/g' launch.jnlp > $new_jnlp
-    sed -i 's/launch.jnlp/localhost.jnlp/g' $new_jnlp
-    sed -i '/<\/title>/a <vendor>eulermind</vendor>' $new_jnlp
-    sed -i '/<\/title>/a <vendor>eulermind</vendor>' $new_jnlp
+    sed 's/${project.url}/project_url/g' launch.jnlp >  $new_jnlp
+
+    sed -i "s/project_url/http:\/\/$host/g"                      $new_jnlp
+    sed -i "s/launch.jnlp/$new_jnlp/g"                  $new_jnlp
+    sed -i '/<\/title>/a <vendor>eulermind</vendor>'    $new_jnlp
 }
 
-make_jnlp localhost
-make_jnlp eulermind
-make_jnlp 123.57.204.59
+pushd eulermind/target/jnlp/
+
+make_jnlp localhost localhost
+make_jnlp eulermind www.eulermind.com
+make_jnlp ali 123.57.204.59
+
+popd
+
+rsync -av eulermind/target/jnlp/ /home/wangxuguang/ninesunqian.github.io/jnlp/
