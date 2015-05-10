7a99b1ba195636e132d2660ff4481f0151a59ac4
wangxuguang <ninesunqian@163.com>
2015-05-10 11:30:18 +0800

    update in 2015-05-10 11:30:18+08:00

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,5 @@
-function make_jnlp ()
-{
-    new_jnlp="$1.jnlp"
-    host="$2"
-
-    sed 's/${project.url}/project_url/g' launch.jnlp >  $new_jnlp
-
-    sed -i "s/project_url/http:\/\/$host/g"                      $new_jnlp
-    sed -i "s/launch.jnlp/$new_jnlp/g"                  $new_jnlp
-    sed -i '/<\/title>/a <vendor>eulermind</vendor>'    $new_jnlp
-}
-
-pushd eulermind/target/jnlp/
-
-make_jnlp localhost localhost
-make_jnlp eulermind www.eulermind.com
-make_jnlp ali 123.57.204.59
-
-popd
-
+cp -f utils/jnlps/*.jnlp eulermind/target/jnlp/ 
 rsync -av eulermind/target/jnlp/ /home/wangxuguang/ninesunqian.github.io/jnlp/
+pushd /home/wangxuguang/ninesunqian.github.io
+git commit -a -m "update in `date --rfc-3339=seconds`"
+popd
