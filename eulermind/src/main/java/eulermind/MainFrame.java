4c11ca6bae086c3eebfe68811038e78009fde200
ninesunqian <ninesunqian@163.com>
2015-05-22 16:54:24 +0800

    open "exmple" map if no last opened map record file

--- eulermind/src/main/java/eulermind/MainFrame.java
+++ eulermind/src/main/java/eulermind/MainFrame.java
@@ -129,6 +129,8 @@ public class MainFrame  extends JFrame {
         String lastOpenedMap = Utils.getLastOpenedMap();
         if (lastOpenedMap != null) {
             openMindDb(lastOpenedMap);
+        } else {
+            openMindDb("example");
         }
     }
 
