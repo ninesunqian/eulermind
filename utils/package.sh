f941c029a21390b106d4b1337109e55fa38459d7
wangxuguang <ninesunqian@163.com>
2015-06-07 14:11:23 +0800

    modify package.sh

--- utils/package.sh
+++ utils/package.sh
@@ -2,7 +2,7 @@
 time=`date +"%F"`
 package_name="eulermind-${time//:/-}"
 (cd ../ && git pull origin && mvn package)  && \
-rm $package_name -rf && \
+rm ${package_name}* -rf && \
 mkdir $package_name &&\
 cp -r ../eulermind/target/jars/ $package_name && \
 cp ../eulermind/target/eulermind-1.0-SNAPSHOT.jar $package_name/jars && \
