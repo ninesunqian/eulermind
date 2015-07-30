118480f4c2e8e5ad3f942852c795af4c01274e51
ninesunqian <ninesunqian@163.com>
2015-07-30 15:32:46 +0800

    save stacktrace to log, if operator has exception

--- eulermind/src/main/java/eulermind/MindController.java
+++ eulermind/src/main/java/eulermind/MindController.java
@@ -423,9 +423,9 @@ public class MindController extends UndoManager {
             }
 
         } catch (Exception e) {
-            m_logger.error("operator exception: " + e.getMessage());
-            m_logger.error("StackTrace: {}", Utils.getThrowableStackTraceString(e));
+            e.printStackTrace();
             JOptionPane.showMessageDialog(null, e.getMessage(), e.getMessage(), JOptionPane.ERROR_MESSAGE);
+            m_logger.warn("operator exception" + e.getMessage());
         }
 
         if (operatorBornMindView != null) {
@@ -456,9 +456,9 @@ public class MindController extends UndoManager {
             }
 
         } catch (Exception e) {
-            m_logger.error("operator exception: " + e.getMessage());
-            m_logger.error("StackTrace: {}", Utils.getThrowableStackTraceString(e));
+            e.printStackTrace();
             JOptionPane.showMessageDialog(null, e.getMessage(), e.getMessage(), JOptionPane.ERROR_MESSAGE);
+            m_logger.warn("operator exception" + e.getMessage());
         }
 
         if (operatorBornMindView != null) {
